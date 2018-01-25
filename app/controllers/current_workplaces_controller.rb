1bb552f66c81f2995a43b9703ab8d6069af3ef16
Nikita Pandey <ankitadangi11@gmail.com>
2018-01-25 19:21:15 +0530

    Working on professional current details

--- app/controllers/current_workplaces_controller.rb
+++ app/controllers/current_workplaces_controller.rb
@@ -16,36 +16,42 @@ class CurrentWorkplacesController < ApplicationController
   def edit
   end
 
-  def create
+   def create
+    @current_workplaces = CurrentWorkplace.all
     @current_workplace = CurrentWorkplace.new(current_workplace_params)
-
+    
     respond_to do |format|
       if @current_workplace.save
-        format.html { redirect_to @current_workplace, notice: 'Current Workplace was successfully created.' }
-        format.json { render :show, status: :created, location: @current_workplace }
+        format.html { redirect_to professional_account_path, notice: 'Past Detail was successfully created.' }
+        format.json { render :show, status: :created, location: @professional_account }
+        format.js {}
       else
         format.html { render :new }
-        format.json { render json: @current_workplace.errors, status: :unprocessable_entity }
+        format.json { render json: @professional_account.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
+    @current_workplace.update_attributes(current_workplace_params)
+
     respond_to do |format|
       if @current_workplace.update(current_workplace_params)
-        format.html { redirect_to @current_workplace, notice: 'Current Workplace was successfully updated.' }
-        format.json { render :show, status: :ok, location: @current_workplace }
+        format.html { redirect_to professional_account_path, notice: 'Past Detail was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @professional_account }
+        format.js
       else
         format.html { render :edit }
-        format.json { render json: @current_workplace.errors, status: :unprocessable_entity }
+        # format.json { render json: @professional_account.errors, status: :unprocessable_entity }
       end
     end
   end
 
+
   def destroy
     @current_workplace.destroy
     respond_to do |format|
-      format.html { redirect_to current_workplaces_url, notice: 'Current Workplacel was successfully destroyed.' }
+      format.html { redirect_to professional_account_url, notice: 'Current Workplacel was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -58,6 +64,6 @@ class CurrentWorkplacesController < ApplicationController
 
    
     def current_workplace_params
-      params.require(:current_workplace).permit(:joined_on,:work_profile, :about_workplace, :designation)
+      params.require(:current_workplace).permit(:joined_on,:work_profile, :about_workplace, :designation, :professional_id)
     end
 end
--- app/controllers/past_details_controller.rb
+++ app/controllers/past_details_controller.rb
@@ -3,18 +3,9 @@ class PastDetailsController < ApplicationController
 
   def index
     @past_details = PastDetail.all
-    respond_to do |format|
-      format.json { render json: @past_detail }
-    end
   end
 
   def show
-    @past_detail = PastDetail.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @past_detail }
-    end
   end
 
   def new
@@ -22,68 +13,52 @@ class PastDetailsController < ApplicationController
   end
 
   def edit
-    @past_detail = PastDetail.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @past_detail }
+      respond_to do |format|
+        format.js
+        format.html
     end
   end
 
   def create
     @past_details = PastDetail.all
-    @past_detail = PastDetail.create(past_detail_params)
+    @past_detail = PastDetail.new(past_detail_params)
     
     respond_to do |format|
       if @past_detail.save
-        format.html { redirect_to professional_account_path(@professional_account), notice: 'Past Detail was successfully created.' }
-        format.json { render :show, status: :created, location: @past_detail }
+        format.html { redirect_to professional_account_path, notice: 'Past Detail was successfully created.' }
+        format.json { render :show, status: :created, location: @professional_account }
         format.js {}
       else
         format.html { render :new }
-        format.json { render json: @past_detail.errors, status: :unprocessable_entity }
+        format.json { render json: @professional_account.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
-    @past_details = PastDetail.all
-    @past_detail = PastDetail.find(params[:id])
     @past_detail.update_attributes(past_detail_params)
 
     respond_to do |format|
       if @past_detail.update(past_detail_params)
-        format.html { redirect_to professional_account_path(@professional_account), notice: 'Past Detail was successfully updated.' }
-        format.json { render :show, status: :ok, location: @past_detail }
+        format.html { redirect_to professional_account_path, notice: 'Past Detail was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @professional_account }
+        format.js
       else
         format.html { render :edit }
-        format.json { render json: @past_detail.errors, status: :unprocessable_entity }
+        # format.json { render json: @professional_account.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def delete
-    @past_detail = PastDetail.find(params[:past_detail_id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @past_detail }
-    end
-  end
-
   def destroy
-    @past_details = PastDetail.all
     @past_detail = PastDetail.find(params[:id])
     @past_detail.destroy
+    # redirect_to professional_account_url
 
     respond_to do |format|
-      format.html
-      format.json { render json: @past_detail }
+      format.html { redirect_to professional_account_url }
+      format.js
     end
-    # respond_to do |format|
-    #   format.html { redirect_to past_details_url, notice: 'Past Detaill was successfully destroyed.' }
-    #   format.json { head :no_content }
-    # end
   end
 
   private
